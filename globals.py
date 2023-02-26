ad9b57fc15507a1247ed97a62b2d0f8eb1897c46
Rico <ricocoder@gmail.com>
2023-02-27 00:15:55 +0100

    Cleaning

---
+++ globals.py
@@ -0,0 +1,1 @@
+acTimePLusInterwal = 0
--- myApp.py
+++ myApp.py
@@ -1,10 +1,10 @@
 from flask import Flask, flash, render_template, request
-import RPi.GPIO as GPIO
-import time as t
+from ReadTemp import read_temp
+from setOutputs import setOutputs
 import webbrowser
 import time
-import glob
 import os
+import globals as g
 
 test = 0
 
@@ -14,7 +14,6 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 pickFolder = os.path.join("static")
 app.config["UPLOAD_FOLDER"]=pickFolder
 mainState = True
-acTimePLusInterwal=0
 pumpEfi = 1
 t1=21
 to1="opis czujnika 1"
@@ -33,9 +32,7 @@ tzo1="boiler"
 tz2=36
 tzo2="ogrzewanie podłogowe"
 
-base_dir = '/sys/bus/w1/devices/'
-device_path = glob.glob(base_dir + '28*')[0] #get file path of sensor
-rom = device_path.split('/')[-1] #get rom name
+
 
 
 
@@ -116,7 +113,8 @@ def result():
             print('-------->>>>>   przycisk wlaczenia pompy zostal wcisniety')
             mainState = True
     c, f = read_temp()
-    pick1, BaseEfiInPercent = setOutputs(mainState, c, pumpEfi)
+    BaseEfiInPercent = setOutputs(mainState, c, pumpEfi)
+    pick1=os.path.join(app.config["UPLOAD_FOLDER"], "6.jpg")
     checkPumpEfi(tz1, c, 5)
     return render_template("index.html", t1=c, to1=to1, t2=t2, to2=to2, t3=t3, to3=to3, t4=t4, to4=to4, t5=t5, to5=to5, t6=t6, to6=to6, tz1=tz1, tz2=tz2, tzo2=tzo2, image1=pick1, pump=BaseEfiInPercent, mainState=mainState)
 
@@ -128,127 +126,9 @@ def settings():
 def history():
     return render_template("history.html")
 
-def read_temp_raw():
-    with open(device_path +'/w1_slave','r') as f:
-        valid, temp = f.readlines()
-    return valid, temp
- 
-def read_temp():
-    valid, temp = read_temp_raw()
 
-    while 'YES' not in valid:
-        time.sleep(0.2)
-        valid, temp = read_temp_raw()
-    
-    pos = temp.index('t=')
-    if pos != -1:
-        temp_string = temp[pos+2:]
-        temp_c = float(temp_string)/1000.0 
-        temp_f = temp_c * (9.0 / 5.0) + 32.0
 
-        return temp_c, temp_f
 
-def setOutputs(state, temp, pumpEfi):
-        SleepTime = 2
-        interval1=60
-        interval2=120
-        BaseEfiInPercent = 100/7
-        GPIO.setmode(GPIO.BCM)
-        pins = [26,5,6,12,25,24,23,22]
-        GPIO.setup(pins[0], GPIO.OUT)
-        GPIO.setup(pins[1], GPIO.OUT)
-        GPIO.setup(pins[2], GPIO.OUT)
-        GPIO.setup(pins[3], GPIO.OUT)
-        GPIO.setup(pins[4], GPIO.OUT)
-        GPIO.setup(pins[5], GPIO.OUT)
-        GPIO.setup(pins[6], GPIO.OUT)
-        GPIO.setup(pins[7], GPIO.OUT)
-        accualTime = t.time()
-        global acTimePLusInterwal
-        print('Aktualny czas:',accualTime)
-        
-        print('acTimePLusInterwal:',acTimePLusInterwal)
-        print('accualTime - acTimePLusInterwal:',accualTime-acTimePLusInterwal)
-        
-        # if accualTime >acTimePLusInterwal +interval1:
-        #     print('Interwal doliczyl do zadanej wartosci nastepuje triger')
-        #     acTimePLusInterwal=accualTime
-        #     print('Interwal doliczyl do zadanej wartosci nastepuje inkrementacja acTimePLusInterwal', acTimePLusInterwal)
-        # print("------------>>>>>>>>jestem w funkcji 'setOutputs' wyswietlam dane state:",state, 'Temp:', temp, 'Wydajnosc pompy:', pumpEfi)
-        pick1=os.path.join(app.config["UPLOAD_FOLDER"], "6.jpg")
-        if state and temp < 25.0:
-            print("------------>>>>>>>>jestem w funkcji 'setOutputs' ustawiam tlo na pompa woda urzytkowa - PonWU.jpg ")
-            pick1=os.path.join(app.config["UPLOAD_FOLDER"], "PonWU.jpg")
-        if state and temp > 25.0 and temp < 30.0:
-            print("------------>>>>>>>>jestem w funkcji 'setOutputs' ustawiam tlo na pompa centralne ogrzewanie PonCO.jpg ")
-            pick1=os.path.join(app.config["UPLOAD_FOLDER"], "PonCO.jpg")
-        if state==False:
-            print("------------>>>>>>>>jestem w funkcji 'setOutputs' ustawiam tlo na pompa wylaczona P_off.jpg ")
-            pick1=os.path.join(app.config["UPLOAD_FOLDER"], "P_off.jpg")
-            
-        if state == True:
-            print("------------>>>>>>>>jestem w funkcji 'setOutputs' sprawdzam warunek state == True", state)
-            GPIO.output(pins[3], GPIO.HIGH)
-
-        else:
-           GPIO.output(pins[3], GPIO.LOW)
-        
-        if pumpEfi==0 and state == True:
-            BaseEfiInPercent = BaseEfiInPercent*pumpEfi
-            print("------------>>>>>>>>jestem w funkcji 'setOutputs' sprawdzam warunek pumpEfi == 0")
-            GPIO.output(pins[0], GPIO.LOW)
-            GPIO.output(pins[1], GPIO.LOW)
-            GPIO.output(pins[2], GPIO.LOW)
-        elif pumpEfi==1 and state == True:
-            BaseEfiInPercent = BaseEfiInPercent*pumpEfi
-            print("------------>>>>>>>>jestem w funkcji 'setOutputs' sprawdzam warunek pumpEfi == 1")
-            GPIO.output(pins[0], GPIO.LOW)
-            GPIO.output(pins[1], GPIO.LOW)
-            GPIO.output(pins[2], GPIO.HIGH)
-        elif pumpEfi == 2 and state == True:
-            BaseEfiInPercent = BaseEfiInPercent*pumpEfi
-            print("------------>>>>>>>>jestem w funkcji 'setOutputs' sprawdzam warunek pumpEfi == 2")
-            GPIO.output(pins[0], GPIO.LOW)
-            GPIO.output(pins[1], GPIO.HIGH)
-            GPIO.output(pins[2], GPIO.LOW)
-        elif pumpEfi == 3 and state == True:
-            BaseEfiInPercent = BaseEfiInPercent*pumpEfi
-            print("------------>>>>>>>>jestem w funkcji 'setOutputs' sprawdzam warunek pumpEfi == 3")
-            GPIO.output(pins[0], GPIO.HIGH)
-            GPIO.output(pins[1], GPIO.LOW)
-            GPIO.output(pins[2], GPIO.LOW)
-        elif pumpEfi == 4 and state == True:
-            BaseEfiInPercent = BaseEfiInPercent*pumpEfi
-            print("------------>>>>>>>>jestem w funkcji 'setOutputs' sprawdzam warunek pumpEfi == 4")
-            GPIO.output(pins[0], GPIO.LOW)
-            GPIO.output(pins[1], GPIO.HIGH)
-            GPIO.output(pins[2], GPIO.HIGH)
-        elif pumpEfi == 5 and state == True:
-            BaseEfiInPercent = BaseEfiInPercent*pumpEfi
-            print("------------>>>>>>>>jestem w funkcji 'setOutputs' sprawdzam warunek pumpEfi == 5")
-            GPIO.output(pins[0], GPIO.HIGH)
-            GPIO.output(pins[1], GPIO.LOW)
-            GPIO.output(pins[2], GPIO.HIGH)
-        elif pumpEfi == 6 and state == True:
-            BaseEfiInPercent = BaseEfiInPercent*pumpEfi
-            print("------------>>>>>>>>jestem w funkcji 'setOutputs' sprawdzam warunek pumpEfi == 6")
-            GPIO.output(pins[0], GPIO.HIGH)
-            GPIO.output(pins[1], GPIO.HIGH)
-            GPIO.output(pins[2], GPIO.LOW)
-        elif pumpEfi == 7 and state == True:
-            BaseEfiInPercent = BaseEfiInPercent*pumpEfi
-            print("------------>>>>>>>>jestem w funkcji 'setOutputs' sprawdzam warunek pumpEfi == 7")
-            GPIO.output(pins[0], GPIO.HIGH)
-            GPIO.output(pins[1], GPIO.HIGH)
-            GPIO.output(pins[2], GPIO.HIGH)
-        else:
-            BaseEfiInPercent = 0
-            GPIO.output(pins[0], GPIO.LOW)
-            GPIO.output(pins[1], GPIO.LOW)
-            GPIO.output(pins[2], GPIO.LOW)
-           
-        
-        return pick1, round(BaseEfiInPercent,1)
 def pumpControl():
     print('jestem w funkcji punpControl')
 
@@ -256,25 +136,24 @@ def checkPumpEfi(t_set: float, t_accual: float, offset: int):
         global pumpEfi
         interval1=60
         interval2=120
-        accualTime = t.time()
-        global acTimePLusInterwal
+        accualTime = time.time()
         print('JEstem w funkcji checkPumpEfi')
         print('Aktualny czas:',accualTime)
-        print('acTimePLusInterwal:',acTimePLusInterwal)
-        print('accualTime - acTimePLusInterwal:',accualTime-acTimePLusInterwal)
+        print('acTimePLusInterwal:', g.acTimePLusInterwal)
+        print('accualTime - acTimePLusInterwal:',accualTime-g.acTimePLusInterwal)
         print('drukuje typ zmiennej: t_set',type(t_set))
         print('drukuje typ zmiennej: t_accual',type(t_accual))
         print('drukuje typ zmiennej: offset',type(offset))
         print('drukuje typ zmiennej: pumpEfi',type(pumpEfi))
-        if accualTime >acTimePLusInterwal + interval1:
+        if accualTime >g.acTimePLusInterwal + interval1:
             print('Interwal doliczyl do zadanej wartosci nastepuje triger')
-            acTimePLusInterwal=accualTime
+            g.acTimePLusInterwal=accualTime
             
             if t_accual > (t_set + offset) and pumpEfi >= 0:
-                print('Interwal doliczyl do zadanej wartosci nastepuje dekrementacja acTimePLusInterwal', acTimePLusInterwal)
+                print('Interwal doliczyl do zadanej wartosci nastepuje dekrementacja acTimePLusInterwal', g.acTimePLusInterwal)
                 pumpEfi=pumpEfi-1
             elif t_accual < (t_set - offset) and pumpEfi < 7:
-                print('Interwal doliczyl do zadanej wartosci nastepuje inkrementacja acTimePLusInterwal', acTimePLusInterwal)
+                print('Interwal doliczyl do zadanej wartosci nastepuje inkrementacja acTimePLusInterwal', g.acTimePLusInterwal)
                 pumpEfi=pumpEfi+1
         
         print("------------>>>>>>>>jestem w funkcji 'checkPumpEfi' wyswietlam dane t_set:",t_set, 't_accual:', t_accual, 'Wydajnosc pompy:', pumpEfi, 'offset:', offset)

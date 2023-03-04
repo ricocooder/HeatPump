600d7b541c33118165f469f035295ae3d621da99
Rico <ricocoder@gmail.com>
2023-03-04 01:21:19 +0100

    Cleaning the code + finishing sensor service + sensorConfig page

--- checkPumpEfi.py
+++ checkPumpEfi.py
@@ -5,25 +5,25 @@ def checkPumpEfi(t_set: float, t_accual: float, offset: int):
         interval1=60
         interval2=120
         accualTime = time.time()
-        print('JEstem w funkcji checkPumpEfi')
-        print('Aktualny czas:',accualTime)
-        print('acTimePLusInterwal:', g.acTimePLusInterwal)
-        print('accualTime - acTimePLusInterwal:',accualTime-g.acTimePLusInterwal)
-        print('drukuje typ zmiennej: t_set',type(t_set))
-        print('drukuje typ zmiennej: t_accual',type(t_accual))
-        print('drukuje typ zmiennej: offset',type(offset))
-        print('drukuje typ zmiennej: pumpEfi',type(g.pumpEfi))
+        #print('JEstem w funkcji checkPumpEfi')
+        #print('Aktualny czas:',accualTime)
+        #print('acTimePLusInterwal:', g.acTimePLusInterwal)
+        #print('accualTime - acTimePLusInterwal:',accualTime-g.acTimePLusInterwal)
+        #print('drukuje typ zmiennej: t_set',type(t_set))
+        #print('drukuje typ zmiennej: t_accual',type(t_accual))
+        #print('drukuje typ zmiennej: offset',type(offset))
+        #print('drukuje typ zmiennej: pumpEfi',type(g.pumpEfi))
         if accualTime >g.acTimePLusInterwal + interval1:
-            print('Interwal doliczyl do zadanej wartosci nastepuje triger')
+            #print('Interwal doliczyl do zadanej wartosci nastepuje triger')
             g.acTimePLusInterwal=accualTime
             
             if t_accual > (t_set + offset) and g.pumpEfi >= 0:
-                print('Interwal doliczyl do zadanej wartosci nastepuje dekrementacja acTimePLusInterwal', g.acTimePLusInterwal)
+                #print('Interwal doliczyl do zadanej wartosci nastepuje dekrementacja acTimePLusInterwal', g.acTimePLusInterwal)
                 g.pumpEfi=g.pumpEfi-1
             elif t_accual < (t_set - offset) and g.pumpEfi < 7:
-                print('Interwal doliczyl do zadanej wartosci nastepuje inkrementacja acTimePLusInterwal', g.acTimePLusInterwal)
+                #print('Interwal doliczyl do zadanej wartosci nastepuje inkrementacja acTimePLusInterwal', g.acTimePLusInterwal)
                 g.pumpEfi=g.pumpEfi+1
         
-        print("------------>>>>>>>>jestem w funkcji 'checkPumpEfi' wyswietlam dane t_set:",t_set, 't_accual:', t_accual, 'Wydajnosc pompy:', g.pumpEfi, 'offset:', offset)
+        #print("------------>>>>>>>>jestem w funkcji 'checkPumpEfi' wyswietlam dane t_set:",t_set, 't_accual:', t_accual, 'Wydajnosc pompy:', g.pumpEfi, 'offset:', offset)
         
         

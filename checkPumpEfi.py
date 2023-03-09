e2c6948ba40bd358fd767c46d6f6280b87a68003
Rico <ricocoder@gmail.com>
2023-03-09 23:27:35 +0100

    Start creating logic form LOGIKA_STEROWANIE.pdf

--- checkPumpEfi.py
+++ checkPumpEfi.py
@@ -1,4 +1,5 @@
 import time
+import datetime as d
 import globals as g
 
 def checkPumpEfi(t_set: float, t_accual: float, offset: int, interval: int, heatObject: int):
@@ -10,3 +11,29 @@ def checkPumpEfi(t_set: float, t_accual: float, offset: int, interval: int, heat
             g.pumpEfi=g.pumpEfi-1
         elif t_accual < (t_set - float(offset[heatObject])) and g.pumpEfi < 7:
             g.pumpEfi=g.pumpEfi+1
+    #logika pracy pompy latem
+    if g.sezon == 'Lato':
+        actualTime = d.datetime.now()
+        actualHour = actualTime.hour
+        dayOfWeek = actualTime.today().weekday()
+        print(g.sezon, actualHour, dayOfWeek)
+        if dayOfWeek > 5:
+            #ustalam harmonogram od poniedzialku do piatku dni tygodnie to zakres 0-6
+            print('zwracam dzien tygodnia', dayOfWeek)
+            if actualHour >=7 and < 9 or actualHour >= 18 and < 22:
+                #wieczorne grzanie
+                print('wieczoren grzanie')
+        else:
+            #pozostale dni tygodnia czyli sobota i niedziela
+            print('zwracam dzien tygodnia', dayOfWeek)
+            if actualHour >= 7 and actualHour < 22:
+                #ustawiam harmonogram od 7 do 22 godziny w niedziele i sobote
+                print('praca od 7 do 22')
+            else:
+                #wylaczam pompe
+                print('pompa wylaczona')
+            
+    #logika pracy pompy zima    
+    else:
+        print("else")
+        

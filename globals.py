04b165e0306dfb955881c5ab5364a602564ec2c3
Rico <ricocoder@gmail.com>
2023-05-21 23:56:49 +0200

    Add coments and discriptions

--- globals.py
+++ globals.py
@@ -1,9 +1,12 @@
 acTimePLusInterwal=0
 pins = [26,5,6,12,25,24,23,16]
+pinsDisc = ['Sterowanie pompy1 (NC)', 'Sterowanie pompy2 (NC)',
+            'Sterowanie pompy3 (NC)', 'Zawor trojdrogowy (NO)',
+            'sterownik piec (NC)', 'zal/wyl 24V (NC)', 'pompa obiegowa (NC)', 'Spare']
 tempPins = [0,0,0,0,0,0,0,0]
 pumpEfi = 1
 diskSpaceList=[]
-heatObject=1
+heatObject=1 #0-nie dziala, 1-boiler, 2-podloga, 
 pumpInterval = [0, 30, 60]
 pumpTempOfset = [0, 2.0, 2.0]
 sezon='Lato'
@@ -12,7 +15,7 @@ BaseEfiInPercentTemp=0
 tempSensFoundNumber=0
 readTemp=[3.14]*64
 readTempTemp=[3.14]*64
-setTemp=[0, 55, 33]
+setTemp=[0, 50, 33]
 pumpIread=0
 pumpI=0
 pumpItemp=0
@@ -20,8 +23,8 @@ pumpV=0
 pumpVread=0
 pumpVtemp=0
 pumpP=0
-trybDiscriptions=['Piec', 'Pompa - boiler', 'Pompa - podlogówka']
-discriptions=['T1 - Piec', 'T2 - Bouler', 'T3 - Pompa wyjscie', 'T4 - Pompa powrot', 'T5 - Temp. zewnetrzna']
+trybDiscriptions=['Zew', 'Pompa - boiler', 'Pompa - podlogówka']
+discriptions=['T1 - Zew', 'T2 - Bouler', 'T4 - Pompa powrot', 'T3 - Pompa wyjscie', 'T5 - Temp. zewnetrzna']
 ledStripDiscription=['[1] Sterowanie bitowe wydajnosci pompy', '[2] Sterowanie bitowe wydajnosci pompy', '[3] Sterowanie bitowe wydajnosci pompy', 
                      '[4] Sterowanie bitowe wydajnosci pompy', '[5] Sterowanie bitowe wydajnosci pompy', '[6] Sterowanie bitowe wydajnosci pompy', 
                      '[7] Sterowanie bitowe wydajnosci pompy', '[8] Sterowanie bitowe wydajnosci pompy']

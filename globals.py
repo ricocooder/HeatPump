21357680cd1616b57618a9326a94698525a8c79f
Rico <ricocoder@gmail.com>
2023-05-24 00:41:58 +0200

    add tables for scheduler

--- globals.py
+++ globals.py
@@ -29,19 +29,30 @@ discriptions=['T1 - Zew', 'T2 - Bouler', 'T4 - Pompa powrot', 'T3 - Pompa wyjsci
 ledStripDiscription=['[1] Sterowanie bitowe wydajnosci pompy', '[2] Sterowanie bitowe wydajnosci pompy', '[3] Sterowanie bitowe wydajnosci pompy', 
                      '[4] Sterowanie bitowe wydajnosci pompy', '[5] Sterowanie bitowe wydajnosci pompy', '[6] Sterowanie bitowe wydajnosci pompy', 
                      '[7] Sterowanie bitowe wydajnosci pompy', '[8] Sterowanie bitowe wydajnosci pompy']
-t1=21
-to1="zasilanie boiler"
-t2=22
-to2="zasilanie podlogowka"
-t3=23
-to3="opis czujnika 3"
-t4=24
-to4="opis czujnika 4"
-t5=25
-to5="opis czujnika 5"
-t6=26
-to6="opis czujnika 6"
-tz1 = 55.1
-tzo1="boiler"
-tz2=36
-tzo2="ogrzewanie podłogowe"
+dni = ['Godzina\Dzien', 'Poniedzialek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota', 'Niedziela']
+godzina = [
+    ['0', 'ON', 'OFF', 'ON', 'ON', 'OFF', 'ON', 'ON'],
+    ['1', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON'],
+    ['2', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON'],
+    ['3', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON'],
+    ['4', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON'],
+    ['5', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON'],
+    ['6', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON'],
+    ['7', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON'],
+    ['8', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON'],
+    ['9', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON'],
+    ['10', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON'],
+    ['11', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON'],
+    ['12', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON'],
+    ['13', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON'],
+    ['14', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON'],
+    ['15', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON'],
+    ['16', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON'],
+    ['17', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON'],
+    ['18', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON'],
+    ['19', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON'],
+    ['20', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON'],
+    ['21', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON'],
+    ['22', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON'],
+    ['23', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON'],   
+]
